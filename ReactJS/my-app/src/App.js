deb2c4332289b072a46e1b9c3963546dd74533b1
Salavuddin555 <salavuddin.sk3@gmail.com>
2022-12-10 07:55:01 +0530

    task completed

--- ReactJS/my-app/src/App.js
+++ ReactJS/my-app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Task1 from './FunctionalComponents/Task1'
+import Task10 from './FunctionalComponents/Task10';
+import Task2 from './FunctionalComponents/Task2'
+import Task3 from './FunctionalComponents/Task3';
+
+import Task4 from './FunctionalComponents/Task4';
+import Task5 from './FunctionalComponents/Task5';
+import Task6 from './FunctionalComponents/Task6';
+import Task7 from './FunctionalComponents/Task7';
+import Task8 from './FunctionalComponents/Task8';
+import Task9 from './FunctionalComponents/Task9';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Task1/>
+      <Task2/>
+      <Task3/>
+      <Task4/>
+      <Task5/>
+      <Task6/>
+      <Task7/>
+      <Task8/>
+      <Task9/>
+      <Task10/>
     </div>
   );
 }
