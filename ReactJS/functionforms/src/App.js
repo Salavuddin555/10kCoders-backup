7a049e5af28df012c2337a81cdea99025c0c4991
Salavuddin555 <salavuddin.sk3@gmail.com>
2022-12-26 06:24:56 +0530

    task completed

--- ReactJS/functionforms/src/App.js
+++ ReactJS/functionforms/src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 // import { User } from './Components/user';
 import { Person } from './Components/person';
-import { Form1, Form1fromserver } from './Components/Form1';
+import { FuncForm2 } from './Components/Form2';
+import { FuncForm3 } from './Components/Form3';
+import { FuncForm4 } from './Components/Form4';
+import { FuncForm5 } from './Components/form5';
+// import { Form1, Form1fromserver, FuncForm1 } from './Components/Form1';
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
       {/* <User/> */}
       {/* <Person/> */}
       {/* <Form1/> */}
-      <Form1fromserver/>
+      {/* <Form1fromserver/> */}
+      {/* <FuncForm2/> */}
+      {/* <FuncForm3/> */}
+      {/* <FuncForm4/> */}
+      <FuncForm5/>
     </div>
   );
 }
--- ReactJS/functionforms/src/Components/Form2.jsx
+++ ReactJS/functionforms/src/Components/Form2.jsx
@@ -1,123 +1,220 @@
 
 
+import React,{ useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
- import React, { useEffect } from "react"
- import axios from 'axios'
-import { useState } from "react";
- const Form2=()=>{
-    const [user,setUser]= useState({
-                id:"",
-                name:"",
-                username:"",
-                email:"",
-                password:"",
-                message:""
+export const FuncForm2 = () => {
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+    message: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const handlechange = (e) => {
+    let newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+
+  const addUser = () => {
+    axios.post("http://localhost:3204/form1", user).then(() => {
+      getAllUsers();
+      clearForm();
     });
-    useEffect(() => {
-    getAllUsers()
-    }, []);
- const [users,setUsers]=useState([])
- const [isEdit,setisEdit]=useState(false)
+  };
 
-    const handleChange=(e)=>{
-            var newUser={...user};
-            newUser[e.target.name]=e.target.value ;
-            setUser(newUser);
-    }
+  const getAllUsers = () => {
+    axios.get("http://localhost:3204/form1").then((respose) => {
+      setUsers(respose.data);
+    });
+  };
 
-    const getAllUsers=()=>{
-        axios.get("http://localhost:3005/form1").then((response)=>{
-            console.log(response.data)
-            setUsers(response.data)
-        })
-    }
-   
+  const clearForm = () => {
+    setUser({
+      id: "",
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmpassword: "",
+      message: "",
+    });
+  };
 
-    const addUser=()=>{
-        axios.post("http://localhost:3005/form1/",user ).then(()=>{
-            getAllUsers()
-            clearForm()
-        })
-    }
+  const editUser = (usr) => {
+    setIsEdit(true);
+    setUser(usr);
+  };
 
-    const clearForm=()=>{
-        setUser( {id:"",
-        name:"",
-        username:"",
-        email:"",
-        password:"",
-        message:""})
-    }
+  const updateUser = () => {
+    axios.put("http://localhost:3204/form1" + user.id, user).then(() => {
+      getAllUsers();
+      setIsEdit(false);
+      clearForm();
+    });
+  };
 
-    const editUser=(usr)=>{
-      setisEdit(true)
-      setUser(usr)
-    }
-
-    const updateUser=()=>{
-        axios.put("http://localhost:3005/form1/"+user.id,user).then(()=>{
-            getAllUsers()
-            setisEdit(false)
-           
-        })
-        clearForm()
-    }
-    const deleteUser=(usr)=>{
-        axios.delete("http://localhost:3005/form1/" +usr.id).then(()=>{
-            getAllUsers()
-        })
-    }
-  return(
-    <div>
-         <form   >
-                 <label >ID:</label>
-                    <input type="text" name="id" value={user.id} disabled  onChange={(e)=>handleChange(e)}/>
-                    <br /><br />
-                    <label >Name:</label>
-                    <input type="text" name="name" value={user.name}  onChange={(e)=>handleChange(e)}/>
-                    <br /><br />
-                    <label >User Name:</label>
-                    <input type="text" name="username" value={user.username} onChange={(e)=>handleChange(e)} />
-                    <br /><br />
-                    <label >Email:</label>
-                    <input type="email" name="email" value={user.email}  onChange={(e)=>handleChange(e)}/>
-                    <br /><br />
-                    <label >Password:</label>
-                    <input type="password" name="password"  value={user.password} onChange={(e)=>handleChange(e)} />
-                    <br /><br />
-                    <label >Message:</label>
-                    <textarea name="message" cols="20" rows="2" value={user.message} onChange={(e)=>handleChange(e)}></textarea>
-                    <br /><br />
-                    {isEdit ? <button  type="button" className="btn btn-primary" onClick={updateUser}>Update User</button> :
-                    <button type="button" className="btn btn-primary" onClick={addUser}>Add User</button>}
-                </form>
-                <hr /> 
-
-                <table className="table">
-                    <thead>
-                        <th>ID</th>
-                       <th>Name</th>
-                       <th>User Name</th>
-                       <th>Email</th>
-                       <th>Password</th>
-                       <th>Message</th>
-                       <th>Edit</th>
-                       <th>Delete</th>
-                    </thead>
-                    <tbody>
-                    {users.map((usr,i)=>
-                    <tr key={i}>
-                        <td>{usr.id}</td>
-                        <td>{usr.name}</td>
-                        <td>{usr.username}</td>
-                        <td>{usr.email}</td>
-                        <td>{usr.password}</td>
-                        <td>{usr.message}</td>
-                        <td>< button type="button" className="btn btn-warning" onClick={()=>editUser(usr,i)}>EDIT</button></td>
-                        <td><button type="button" className="btn btn-danger " onClick={()=>deleteUser(usr,i)} >DELETE</button></td>
-                    </tr>)} 
-                    </tbody>
-                </table> 
-    </div>
-  )
+  const deleteUser = (usr) => {
+    axios.delete("http://localhost:3204/form1" + usr.id).then(() => {
+      getAllUsers();
+    });
+    
+  }
+    return (
+      <div className="container">
+        <form>
+          <label htmlFor="">ID</label>
+          <input
+            type="text"
+            name="id"
+            value={user.id}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />
+          <br />
+          <br />
+          <label htmlFor="">Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          <label htmlFor="">Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          <label htmlFor="">email:</label>
+          <input
+            type="text"
+            name="email"
+            value={user.email}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          <label htmlFor="">Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          <label htmlFor="">Confirm Password:</label>
+          <input
+            type="text"
+            name="confirmpassword"
+            value={user.confirmpassword}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          <label htmlFor="">Message:</label>
+          <input
+            type="text"
+            name="message"
+            value={user.message}
+            onChange={(e) => {
+              handlechange(e);
+            }}
+          />{" "}
+          <br />
+          <br />
+          {isEdit ? (
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={updateUser}
+            >
+              Update user
+            </button>
+          ) : (
+            <button type="button" className="btn btn-primary" onClick={addUser}>
+              Add user
+            </button>
+          )}
+        </form>
+        <br />
+        <hr />
+        <br />
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>email</th>
+              <th>password</th>
+              <th>confirmpassword</th>
+              <th>message</th>
+              <th>edit</th>
+              <th>delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((usr, i) => (
+              <tr key={i}>
+                <td>{usr.name}</td>
+                <td>{usr.username}</td>
+                <td>{usr.email}</td>
+                <td>{usr.password}</td>
+                <td>{usr.confirmpassword}</td>
+                <td>{usr.message}</td>
+                <td>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                      editUser(usr);
+                    }}
+                  >
+                    edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      deleteUser(usr);
+                    }}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    ); 
+    
 }
+
